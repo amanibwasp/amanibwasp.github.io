427deb4407e35bd5ddaa28da6b13de04ce3eada5
amanibwasp <amanibwasp@gmail.com>
2023-09-11 20:05:19 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//console.log("Script loaded successfully");
+//
+//$(document).ready(function() {
+//    $('.header__burger').click(function(event) {
+//        $('.header__burger,.header__menu').toggleClass('active');
+//        $('body').toggleClass('lock');
+//    });
+//});
+//
+//
+//function autoSlide() {
+//    var slides = document.getElementsByClassName("slides")[0].children;
+//    var radios = document.getElementsByName("r");
+//    var index = 0;
+//    var interval = setInterval(function() {
+//        radios[index].checked = true;
+//        index++;
+//        if (index >= radios.length) {
+//            index = 0;
+//        }
+//    }, 5000); // интервал в миллисекундах
+//}
+//
+//window.onload = function() {
+//    autoSlide();
+//}
+
+
+let burger_button = document.querySelector(".hamburger-button").addEventListener("click", ()=>
+{
+    document.querySelector(".nav-menu-burger").classList.toggle("active")
+})
+
+
+
